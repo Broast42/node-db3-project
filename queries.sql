1c12779e0e0155f691a4f0ab68ed4aa026d03b48
rdowd <rjarrettdowd@yahoo.com>
2020-04-28 13:45:07 -0400

    Added sql statements in quiries.sql folder

--- queries.sql
+++ queries.sql
@@ -1,9 +1,32 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+    SELECT  p.'Id' "ProductId", p.'ProductName', c.'CategoryName'
+    FROM "Product" AS p
+    JOIN "Category" AS c
+    ON p.'Id' = c.'Id';
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+    SELECT o.'Id', s.'CompanyName'
+    FROM "Order" AS o
+    JOIN "Shipper" AS s
+    ON o."ShipVia" = s.Id
+    WHERE o."OrderDate" < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+    SELECT p."ProductName", od."Quantity"
+    FROM "OrderDetail" AS od 
+    JOIN "Product" AS p
+    ON od."ProductId" = p."Id"
+    JOIN "Order" AS o
+    ON o."Id" = od."OrderId"
+    WHERE o.'id' = 10251
+    ORDER BY p."ProductName"
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+    SELECT o."Id" "OrderId", c."CompanyName", e."LastName" "EmployeeLastName"
+    FROM "Order" AS o
+    JOIN "Customer" AS c
+    ON o."CustomerId" = c."Id"
+    JOIN "Employee" AS e
+    ON o."EmployeeId" = e."Id"
