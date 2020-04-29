ceaf2842a603ee25358722c43f677e292bf86a03
rdowd <rjarrettdowd@yahoo.com>
2020-04-28 21:28:37 -0400

    All functions are compete and first stretchsql querry complete.

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@
     FROM "Order" AS o
     JOIN "Shipper" AS s
     ON o."ShipVia" = s.Id
-    WHERE o."OrderDate" < "2012-08-09"
+    WHERE o."OrderDate" < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
     SELECT p."ProductName", od."Quantity"
@@ -21,7 +21,7 @@
     JOIN "Order" AS o
     ON o."Id" = od."OrderId"
     WHERE o.'id' = 10251
-    ORDER BY p."ProductName"
+    ORDER BY p."ProductName";
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
     SELECT o."Id" "OrderId", c."CompanyName", e."LastName" "EmployeeLastName"
@@ -29,4 +29,14 @@
     JOIN "Customer" AS c
     ON o."CustomerId" = c."Id"
     JOIN "Employee" AS e
-    ON o."EmployeeId" = e."Id"
+    ON o."EmployeeId" = e."Id";
+
+
+-- Stretch problems from SQL Try Editor at W3Schools.com
+
+-- Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 8 records.
+    SELECT c."CategoryName", COUNT(p."ProductID") "count"
+    FROM "Products" AS p
+    JOIN "Categories" AS c
+    ON p."CategoryID" = c."CategoryID"
+    GROUP BY c."CategoryID";
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,6 +4,54 @@ function find(){
     return db("schemes")
 }
 
+function findById(id){
+    return db("schemes").where("id", id).first()
+}
+
+function findSteps(id){
+    return db("steps as s")
+        .join("schemes as sc", "s.scheme_id", "sc.id")
+        .where("sc.id", id)
+        .select("s.id", "sc.scheme_name", "s.step_number", "s.instructions")
+}
+
+async function add(scheme){
+    try{
+       const [id] = await db("schemes").insert(scheme)
+       return db("schemes").where("id", id).first()
+
+    } catch(err){
+        return err
+    }
+    
+
+}
+
+async function update(changes, id){
+    try{
+        await db("schemes").where("id", id).update(changes)
+        return db("schemes").where("id", id).first()
+    }catch(err){
+        return err
+    }
+}
+
+async function remove(id){
+    try{
+        const [deleted] = await db("schemes").where("id", id)
+        await db("schemes").where("id", id).del()
+        return deleted
+    }catch(err){
+        return err
+    }
+    
+}
+
 module.exports = {
     find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
 }
